de303448dc27fe93601c489aed8841f0e41b600c
Ivan Kerin <ikerin@gmail.com>
2025-05-01 00:02:24 +0530

    Bump reported version

--- packages/directus-typegen-cli/src/directus-typegen.ts
+++ packages/directus-typegen-cli/src/directus-typegen.ts
@@ -3,6 +3,6 @@ import snapshot from './snapshot.js';
 import server from './server.js';
 export default createCommand('directus-typegen')
   .description('Generate TypeScript types from Directus schema snapshots')
-  .version('0.1.7')
+  .version('0.2.0')
   .addCommand(snapshot())
   .addCommand(server());
